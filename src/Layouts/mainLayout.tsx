989b50b719f208b40da1603e5e446935f0a31437
Alexis C. <alex@gauzz.com.mx>
2023-11-11 11:38:28 -0600

    base beta 2.0

--- src/Layouts/mainLayout.tsx
+++ src/Layouts/mainLayout.tsx
@@ -3,8 +3,8 @@ import Sidebar from "../components/sidebar";
 
 export default function MainLayout() {
   return (
-    <div className="w-full h-[100dvh] flex flex-row bg-black">
-      <div className="">
+    <div className="w-full h-[100dvh] flex flex-row ">
+      <div>
         <Sidebar />
       </div>
       <div className="grow bg-stone-400">
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,15 @@
+import { PiAlignLeftBold } from "react-icons/pi";
+
 export default function Sidebar() {
-  return <div>sidebar</div>;
+  return (
+    <div className="grow test flex flex-col items-end px-5 py-8">
+      <div>a</div>
+      <div className="flex flex-row items-center gap-2 px-2 py-1 rounded-md bg-slate-400">
+        <div className="bg-white p-1 rounded-full text-sm">
+          <PiAlignLeftBold />
+        </div>
+        <p className="text-xs font-medium">GanttChat</p>
+      </div>
+    </div>
+  );
 }
