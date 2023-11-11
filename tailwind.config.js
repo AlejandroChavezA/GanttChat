197f4330648cb60e0ed83203a0da26066ecbddf7
Isaay Sosa Hernández <93836824+iso11ml@users.noreply.github.com>
2023-11-11 11:31:59 -0600

    paleta_colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        main:{
+          100: '#0A2F51',
+          200: '#0E4D64',
+          300: '#137177',
+          400: '#188977',
+          500: '#1D9A6C',
+          600: '#39A96B',
+          700: '#56B870',
+          800: '#74C67A',
+          900: '#99D492',
+          950: '#BFE1B0',
+          default: '#1D9A6C'
+        }
+      }
+    },
   },
   plugins: [],
 };
